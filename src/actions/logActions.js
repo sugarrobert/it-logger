b570762467c167e26dc5d22fdab6d2e164c65d38
sugarrobert <robert.sugar@hotmail.com>
2021-01-08 15:00:44 +0100

    Add Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
 
 // export const getLogs = () => {
 //   return async (dispach) => {
@@ -24,6 +24,32 @@ export const getLogs = () => async (dispach) => {
 
     dispach({
       type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispach({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+//Add new log
+export const addLog = (log) => async (dispach) => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispach({
+      type: ADD_LOG,
       payload: data,
     });
   } catch (err) {
